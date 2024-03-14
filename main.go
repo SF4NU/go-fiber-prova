18e5eec9bbd3a0338498d1e9f574c33620ac2dc4
SF4NU <mariusmarius200121@gmail.com>
2024-03-14 14:52:51 +0100

    aggiunto cors

--- main.go
+++ main.go
@@ -25,6 +25,17 @@ func main() {
 
 	app := fiber.New()
 
+	app.Use(func(c *fiber.Ctx) error {
+		c.Set("Access-Control-Allow-Origin", "*") 
+		c.Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE") 
+		c.Set("Access-Control-Allow-Headers", "Content-Type, Authorization") 
+		if c.Method() == "OPTIONS" {
+			c.Status(fiber.StatusOK)
+			return nil
+		}
+		return c.Next()
+	})
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Todo-api")
 	})
