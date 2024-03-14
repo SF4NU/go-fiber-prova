34994fc7ca298475feff7632d1fe5e4dabfed6ee
SF4NU <mariusmarius200121@gmail.com>
2024-03-14 14:13:58 +0100

    prova4

--- main.go
+++ main.go
@@ -1,38 +1,13 @@
 package main
 
-import (
-
-	"github.com/gofiber/fiber/v2"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-)
-
-type Task struct {
-	ID          uint   `gorm:"primaryKey"`
-	Description string `json:"description"`
-	Completed   bool   `json:"completed"`
-	Deadline    string `json:"deadline"`
-}
+import "github.com/gofiber/fiber/v2"
 
 func main() {
-	// if err := godotenv.Load(); err != nil {
-	// 	panic("Error loading .env file")
-	// }
-	// dbKey := os.Getenv("DB_KEY")
+    app := fiber.New()
 
-	dsn := "postgres://ryfljouh:dhohPc3uydQ006hRhGsulapMesD4MLFd@dumbo.db.elephantsql.com/ryfljouh"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic("db not connected")
-	}
+    app.Get("/", func(c *fiber.Ctx) error {
+        return c.SendString("Hello, World!")
+    })
 
-	db.AutoMigrate(&Task{})
-
-	app := fiber.New()
-
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
-
-	app.Listen(":3000")
+    app.Listen(":3000")
 }
