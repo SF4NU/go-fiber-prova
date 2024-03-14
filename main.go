6415578c049078d6208ba1a4ea075b543dc5f2e9
SF4NU <mariusmarius200121@gmail.com>
2024-03-14 14:31:06 +0100

    prova6

--- main.go
+++ main.go
@@ -4,17 +4,39 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+type Task struct {
+	ID          uint   `gorm:"primaryKey"`
+	Description string `json:"description"`
+	Completed   bool   `json:"completed"`
+	Deadline    string `json:"deadline"`
+}
+
 func main() {
+	dsn := "postgres://ryfljouh:dhohPc3uydQ006hRhGsulapMesD4MLFd@dumbo.db.elephantsql.com/ryfljouh"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("db not connected")
+	}
+	db.AutoMigrate(&Task{})
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.SendString("Todo-api")
 	})
 
 	app.Get("/home", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World Again!")
+	})
+
+	app.Get("/tasks", func(c *fiber.Ctx) error {
+		var tasks []Task
+		db.Find(&tasks)
+		return c.Status(fiber.StatusOK).JSON(tasks)
 	})
 
 	port := os.Getenv("PORT")
